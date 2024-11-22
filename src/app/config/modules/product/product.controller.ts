e99b72da4f4b5f303cfa71699a447b13ee70ee03
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-11-22 16:22:47 +0600

    product basic crud operation complete

--- src/app/config/modules/product/product.controller.ts
+++ src/app/config/modules/product/product.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { ProductServices } from './product.service';
 import productValidationSchema from './product.validation';
-
+// create a bike
 const createProduct = async (req: Request, res: Response) => {
   try {
     const productData = req.body;
@@ -34,6 +34,86 @@ const createProduct = async (req: Request, res: Response) => {
   }
 };
 
+// get all bike
+const getAllProduct = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProductDB();
+    res.status(200).json({
+      success: true,
+      message: 'Bikes retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Validation failed',
+      success: false,
+      error,
+    });
+  }
+};
+
+// get single bike by id
+const getSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.productId;
+    const result = await ProductServices.getSingleProductDB(id);
+    res.status(200).json({
+      success: true,
+      message: 'Bike retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Validation failed',
+      success: false,
+      error,
+    });
+  }
+};
+
+//update single bike by id
+const updateSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.productId;
+    const data = req.body;
+    const result = await ProductServices.updateProductDB(id, data);
+    res.status(200).json({
+      success: true,
+      message: 'Bike updated successfully',
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Validation failed',
+      success: false,
+      error,
+    });
+  }
+};
+
+//delete single bike by id
+const deleteSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.productId;
+    await ProductServices.deleteProductDB(id);
+    res.status(200).json({
+      success: true,
+      message: 'Bike deleted successfully',
+      result: {},
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Validation failed',
+      success: false,
+      error,
+    });
+  }
+};
+
 export const ProductController = {
   createProduct,
+  getAllProduct,
+  getSingleProduct,
+  updateSingleProduct,
+  deleteSingleProduct,
 };
--- src/app/config/modules/product/product.service.ts
+++ src/app/config/modules/product/product.service.ts
@@ -6,6 +6,32 @@ const createProductIntoDB = async (productData: TProduct) => {
   return result;
 };
 
+const getAllProductDB = async () => {
+  const result = await Product.find();
+  return result;
+};
+
+const getSingleProductDB = async (productId: string) => {
+  const result = await Product.findById(productId);
+  return result;
+};
+
+const updateProductDB = async (productId: string, data: Partial<TProduct>) => {
+  const result = await Product.findByIdAndUpdate(productId, data, {
+    new: true,
+  });
+  return result;
+};
+
+const deleteProductDB = async (productId: string) => {
+  const result = await Product.findByIdAndDelete(productId);
+  return result;
+};
+
 export const ProductServices = {
   createProductIntoDB,
+  getAllProductDB,
+  getSingleProductDB,
+  updateProductDB,
+  deleteProductDB,
 };
