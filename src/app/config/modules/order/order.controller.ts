1e3650a1136c8592e1487c2dd7dc1974759a30fa
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-11-22 23:52:26 +0600

    fix eslint

--- src/app/config/modules/order/order.controller.ts
+++ src/app/config/modules/order/order.controller.ts
@@ -14,6 +14,7 @@ const createOrder = async (req: Request, res: Response) => {
       message: 'Order created successfully!',
       data: result,
     });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     res.status(400).json({
       success: false,
