4a76b4a23631948fd082ede12856d75e7e941d5d
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-01-27 17:26:50 +0600

    refersh token some validation add

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,11 +1,29 @@
 import express from 'express';
 import { UserControllers } from './user.controller';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.constant';
 
 const router = express.Router();
 
-router.post('/', UserControllers.registerUser);
-router.get('/:id', UserControllers.getUserById);
-router.put('/:id', UserControllers.updateUserById);
-router.delete('/:id', UserControllers.deleteUserById);
+router.post(
+  '/',
+  auth(USER_ROLE.admin, USER_ROLE.customer),
+  UserControllers.registerUser,
+);
+router.get(
+  '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.customer),
+  UserControllers.getUserById,
+);
+router.put(
+  '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.customer),
+  UserControllers.updateUserById,
+);
+router.delete(
+  '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.customer),
+  UserControllers.deleteUserById,
+);
 
 export const UserRoute = router;
